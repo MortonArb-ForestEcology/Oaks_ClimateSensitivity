ea35174eb7c5c7886944381095280f90c9320160
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2020-01-23 13:59:47 -0600

    Aggregated data in prep for ordination
    These few lines took a few hours because I couldn't figure out why aggregate was giving innaccurate values since it was automaticcaly converting factors to numeric which changes the values. Had to break up the process and slowly work through but found the problem and rememerbed I already learned about that conflict. Fun stuff but we made it

--- Oak Prioritizing.R
+++ Oak Prioritizing.R
@@ -37,5 +37,22 @@ colnames(bien.dat)
 
 #removing oak species we aren't concerned with from BIEN
 bien.mod <- bien.dat[bien.dat$species %in% specieslist, ]
+bien.mod <- subset(bien.mod, select=c(1:4))
 
+#Converting to character then number YOU MUST CONVERT TO CHARCTER FIRST OR VALUES CHANGE
+bien.mod$trait_value <- as.numeric(as.character(bien.mod$trait_value))
+
+bien.agg <- aggregate(trait_value~species+trait_name, data=bien.mod, median)
+View(bien.agg)
+#currently this removes non numeric traits e.g. flower color, whole plant dispersal syndrome
+
+
+#test case
+bien.modt <- bien.dat[bien.dat$species == "Quercus acutissima", ]
+bien.modt <- bien.modt[bien.modt$trait_name == "seed mass", ]
+View(bien.modt)
+
+bien.modt$trait_value <- as.numeric(as.character(bien.modt$trait_value))
+bien.aggt <- aggregate(trait_value~trait_name+species, data=bien.modt, mean)
+View(bien.aggt)
 
