879b47f44c8d7c87c1329cff25f217ddd718465e
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2020-01-24 15:36:22 -0600

    Code to download maps
    Added a section for downloading range maps from BIEN's database. Should only need to do it once but figured these should be kept together

--- BIEN_download.R
+++ BIEN_download.R
@@ -13,8 +13,14 @@ traits <- paste(genus, species, sep=" ")
 qa <- BIEN_trait_species(traits)
 View(qa)
 
+#download step to get all the range maps (using specieslist from prioritizing script since this should only run 1 time)
+ranges <- as.character(specieslist)
+qr <- BIEN_ranges_species(ranges)
+View(qr)
+
+
 filename <- paste(genus, "_", species, "_traits.csv", sep="")
-write.csv(qa, file.path(dr, file = filename), row.names=FALSE)
+write.csv(qr, file.path(dr, file = filename), row.names=FALSE)
 
 #removing duplicates (same species and same trait) this does merge measurements so only used to reduce space.
 mod <- qa[!duplicated(qa$trait_name),]
