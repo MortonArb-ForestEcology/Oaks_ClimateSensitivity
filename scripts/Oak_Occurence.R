341d63b739bf7c0e7cb09260c8d1c89de2e9ecad
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2020-02-19 16:30:19 -0600

    Modifying daymet data for values of interest
    Currently takes one point and can get the annual min and max of temp as well as the first and last freeze. Just realized how I can make the freeze loops into 1 by counting 1 past the last freeze. Thats the next step.

--- scripts/Oak_Occurence.R
+++ scripts/Oak_Occurence.R
@@ -1,32 +1,77 @@
 #This script is to download Daymet data for any single points of interest.
+library(dplyr)
 
 #setting file path
-
-path.save <- "C:/Users/lucie/Documents/"
+path.save <- "G:/My Drive/Oaks_ClimateSensitivity/Occurrence/"
 
 #retrieving the occurence points from BIEN
-qo <- BIEN::BIEN_occurrence_species("Quercus acutissima", only.new.world = FALSE) #, native.status = TRUE, natives.only = FALSE)
+#qo <- BIEN::BIEN_occurrence_species("Quercus acutissima", only.new.world = FALSE) #, native.status = TRUE, natives.only = FALSE)
+
+
+setwd(path.save)
+species <- "Q_georgiana"
+ystart <- 1980
+yend <- 2018
+
+
+#loading species list of occurence points
+q.occur <- read.csv(paste(species, "_spatial_data.csv", sep=""))
 
 #Subsetting to only include lat and long (and for now the first rows to make testing easier)
-q.lat <- qo[(c=1:5),(c=2:3)]
+q.lat <- q.occur[(c=1),(c=8:9)]
 
 #creating a proxy "site" column because the batch function needs it
 q.lat$site <- "Daymet"
 q.lat <- q.lat[,c(3,1,2)]
 
 #Writing the csv file of lat and longs because batch function needs to read a file instea of a dataframe
-write.csv(q.lat, file.path(path.save, file = "test_lat.csv"), row.names=FALSE)
+#write.csv(q.lat, file.path(path.save, file = "test_lat.csv"), row.names=FALSE)
 
 #Downloading all of the damet data for each point. Internal =TRUE means it creates a nested list. Set false to actually download a file
-lat.df <- daymetr::download_daymet_batch(file_location = "test_lat.csv",
-                      start = 2000,
-                      end = 2010,
-                      internal = TRUE)
+#lat.df <- daymetr::download_daymet_batch(file_location = "test_lat.csv",
+                      #start = ystart,
+                      #end = yend,
+                      #internal = TRUE)
 
+daymet.df <- daymetr::download_daymet(lat = 34.8101, lon = -86.9814, start = ystart, end = yend, internal = TRUE, simplify = TRUE)
 
-#df <- download_daymet(lat = 37.08000, lon = -81.92000, start = 2000, end = 2010, internal = TRUE, simplify = TRUE)
+#calculating annual max temperature for each point
+daymet.tmax <- daymet.df %>% group_by(year, measurement) %>% summarise(max = max(value))
+daymet.tmax <- daymet.tmax[(daymet.tmax$measurement == "tmax..deg.c."),]
 
+#calculating annual min temeperature for each point
+daymet.tmin <- daymet.df %>% group_by(year, measurement) %>% summarise(min = min(value))
+daymet.tmin <- daymet.tmin[(daymet.tmin$measurement == "tmin..deg.c."),]
 
+#calculating day of first hard freeze (might work these into one loop but unsure how right now)
+daymet.frz <- daymet.df[(daymet.df$measurement == "tmin..deg.c." & daymet.df$value <=0),]
 
+year.frz <- ystart
+rows <- 1
+for(i in rows:nrow(daymet.frz)){
+  if(daymet.frz[i, "year"] == year.frz){
+    if(daymet.frz[i, "yday"] > 171){
+      daymet.frz[i, "first.freeze"] <- TRUE
+      year.frz <- year.frz + 1
+    } 
+  }
+  rows <- rows+1
+}
+ffreeze.df <- daymet.frz[!(is.na(daymet.frz$last.freeze)),]
 
+#calculating day of last hard freeze (make it add to next freeze and this can be one loop)
+daymet.lfrz <- daymet.df[(daymet.df$measurement == "tmin..deg.c." & daymet.df$value <=0),]
 
+year.lfrz <- ystart
+l.rows <- 1
+for(i in l.rows:nrow(daymet.lfrz)){
+  if(daymet.lfrz[i, "year"] == year.lfrz){
+    if(daymet.lfrz[i, "yday"] < 171){
+        daymet.lfrz[i, "last.freeze"] <- 1
+        daymet.lfrz[i-1, "last.freeze"] <- 0
+    } else{year.lfrz <- year.lfrz+1}
+  }
+  l.rows <- l.rows+1
+}
+
+lfreeze.df <- daymet.lfrz[(daymet.lfrz$last.freeze == 1),]
