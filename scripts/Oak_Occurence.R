d319aa6c47c6eb60d43146d1c51b2946ac812056
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2020-02-24 08:55:37 -0600

    Shifting to working with multiple lat and long points
    Initial changes to start testing cases with multiple points. I have multiple known locations where I will need to change things but I want to make sure I can read the data in properly so my changes reflect what format I go with. Currently struggling with finding a usable format

--- scripts/Oak_Occurence.R
+++ scripts/Oak_Occurence.R
@@ -18,20 +18,24 @@ yend <- 2018
 q.occur <- read.csv(paste(species, "_spatial_data.csv", sep=""))
 
 #Subsetting to only include lat and long (and for now the first rows to make testing easier)
-q.lat <- q.occur[(c=1),(c=8:9)]
+q.lat <- q.occur[(c=1:3),(c=8:9)]
 
 #creating a proxy "site" column because the batch function needs it
 q.lat$site <- "Daymet"
 q.lat <- q.lat[,c(3,1,2)]
 
 #Writing the csv file of lat and longs because batch function needs to read a file instea of a dataframe
-#write.csv(q.lat, file.path(path.save, file = "test_lat.csv"), row.names=FALSE)
+write.csv(q.lat, file.path("C:/Users/lucie/Documents/", file = "test_lat.csv"), row.names=FALSE)
 
 #Downloading all of the damet data for each point. Internal =TRUE means it creates a nested list. Set false to actually download a file
-#lat.df <- daymetr::download_daymet_batch(file_location = "test_lat.csv",
-                      #start = ystart,
-                      #end = yend,
-                      #internal = TRUE)
+lat.list <- daymetr::download_daymet_batch(file_location = "test_lat.csv",
+                      start = ystart,
+                      end = yend,
+                      internal = T)
+
+lat.df <- do.call(rbind.data.frame, lapply(lat.list, as.data.frame, stringsAsFactors = F))
+
+
 
 daymet.df <- daymetr::download_daymet(lat = 34.8101, lon = -86.9814, start = ystart, end = yend, internal = TRUE, simplify = TRUE)
 
